f6bb343b00d13bcbec996eee9b0e03e806fd62f3
Shreedutt <shreedutt77@gmail.com>
2022-08-20 09:05:02 +0530

    light is added to scene

--- js/game.js
+++ js/game.js
@@ -46,6 +46,27 @@ function createScene() {
 	window.addEventListener("resize", handleWindowResize, false);
 }
 
+// Light Variables
+const hemisphereLight = new THREE.HemisphereLight(0xaaaaaa, 0x000000, 0.9); // the first parameter is the sky color, the second parameter is the ground color
+const shadowLight = new THREE.DirectionalLight(0xffffff, 0.9); // The sun
+
+// Create Lights
+function createLights() {
+	shadowLight.position.set(150, 350, 350);
+	shadowLight.castShadow = true;
+	shadowLight.shadow.camera.left = -400;
+	shadowLight.shadow.camera.right = 400;
+	shadowLight.shadow.camera.top = 400;
+	shadowLight.shadow.camera.bottom = -400;
+	shadowLight.shadow.camera.near = 1;
+	shadowLight.shadow.camera.far = 1000;
+	shadowLight.shadow.mapSize.width = 2048;
+	shadowLight.shadow.mapSize.height = 2048;
+
+	scene.add(hemisphereLight);
+	scene.add(shadowLight);
+}
+
 function initialize(event) {
 	createScene();
 }
