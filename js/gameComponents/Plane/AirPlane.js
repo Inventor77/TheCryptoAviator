81b53ac5194fcc0bb454710a4206b4c0cb1cbed4
Shreedutt <shreedutt77@gmail.com>
2022-08-20 17:29:53 +0530

    created AirPlane component and added sub component related to airplane to it.

---
+++ js/gameComponents/Plane/AirPlane.js
@@ -0,0 +1,30 @@
+import { Colors } from "../../utils/colors.js";
+import Cockpit from "./Cockpit.js";
+import Engine from "./Engine.js";
+import Propeller from "./Propeller.js";
+import SideWing from "./SideWing.js";
+import Tail from "./Tail.js";
+
+export default function AirPlane() {
+	this.mesh = new THREE.Object3D();
+
+	// Create the cabin
+	const cockpit = new Cockpit();
+	this.mesh.add(cockpit.mesh);
+
+	// Create the engine
+	const engine = new Engine();
+	this.mesh.add(engine.mesh);
+
+	// Create the tail
+	const tail = new Tail();
+	this.mesh.add(tail.mesh);
+
+	// Create the wing
+    const sideWing = new SideWing();
+	this.mesh.add(sideWing.mesh);
+
+	// propeller
+    const propeller = new Propeller();
+    this.mesh.add(propeller.mesh);
+}
