9a4c91b1d206b2ec9d157258628ce722c2d32b17
Shreedutt <shreedutt77@gmail.com>
2022-08-21 17:37:32 +0530

    added sea color variations

--- js/utils/colors.js
+++ js/utils/colors.js
@@ -6,3 +6,12 @@ export const Colors = {
 	brownDark: 0x23190f,
 	blue: 0x68c3c0,
 };
+
+export const COLOR_SEA_LEVEL = [
+	0x68c3c0, // hsl(178deg 43% 59%)
+	0x47b3af, // hsl(178deg 43% 49%)
+	0x398e8b, // hsl(178deg 43% 39%)
+	0x2a6a68, // hsl(178deg 43% 29%)
+	0x1c4544, // hsl(178deg 43% 19%)
+	0x0d2120, // hsl(178deg 43% 09%)
+];
